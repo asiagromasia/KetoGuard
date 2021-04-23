3eafcf3c59bfabc43598824fe87fc689bf6dc2ac
Asia <Joanna.gromadzka@seattlecentral.edu>
2021-04-23 14:58:54 -0700

    small adjustmen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Czesc Asia!</Text>
+      <Text>Czesc Asia!!</Text>
       <StatusBar style="auto" />
     </View>
   );
